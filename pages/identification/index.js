a029a34f57cdf1cde80dcb04f51797fe58532aaa
0badran <badran7299@yahoo.com>
2024-04-10 20:11:57 +0200

    Merage main into badran

--- pages/identification/index.js
+++ pages/identification/index.js
@@ -11,33 +11,23 @@ import { auth, firestore } from "@/firebase";
 import Image from "next/image";
 import topLogo from "@/public/1.png";
 import bottomLogo from "@/public/bottom-logo.png";
-<<<<<<< HEAD
-import { useState, useEffect } from "react";
-=======
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
->>>>>>> main
 
 function Login_signup() {
   const router = useRouter();
   const provider = new GoogleAuthProvider();
-<<<<<<< HEAD
-  const [storageProducts, setStorageProducts] = useState(null);
-=======
->>>>>>> main
   const [spinner, setSpinner] = useState(false);
+  const [storageProducts, setStorageProducts] = useState([]);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-<<<<<<< HEAD
   useEffect(() => { setStorageProducts(JSON.parse(localStorage.getItem("cart"))) }, []);
-=======
   const { t } = useTranslation("login");
 
->>>>>>> main
   async function loginOrSignup({ email }) {
     const isExist = await fetchSignInMethodsForEmail(auth, email);
     if (isExist.length) {
@@ -48,7 +38,6 @@ function Login_signup() {
   }
 
   function loginWithGoogle() {
-<<<<<<< HEAD
     signInWithPopup(auth, provider).then((result) => {
       // The signed-in user info.
       const userID = result.user.uid;
@@ -76,7 +65,6 @@ function Login_signup() {
       // Handle Errors here.
       console.log("Error: ", error);
     });
-=======
     signInWithPopup(auth, provider)
       .then((result) => {
         // The signed-in user info.
@@ -113,15 +101,11 @@ function Login_signup() {
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
       });
->>>>>>> main
   }
 
   return (
     <>
       {spinner ? <Spinner /> : ""}
-<<<<<<< HEAD
-      <Card className={spinner ? "flex flex-col items-center mt-10 relative opacity-25" : "flex flex-col items-center mt-10 relative"} shadow={false}>
-=======
       <Card
         className={
           spinner
@@ -130,7 +114,6 @@ function Login_signup() {
         }
         shadow={false}
       >
->>>>>>> main
         <div className="flex flex-col items-center w-[30rem]">
           <Image
             width="70"
@@ -140,19 +123,12 @@ function Login_signup() {
             alt="logo-image"
           />
           <Typography variant="h3" color="black">
-<<<<<<< HEAD
-            Welcome to Jumia
-          </Typography>
-          <Typography color="black" className="mt-1 text-center">
-            Type your e-mail or phone number to log in or create a Jumia account.
-=======
             {t("Welcome to Jumia")}
           </Typography>
           <Typography color="black" className="mt-1 text-center">
             {t(
               "Type your e-mail or phone number to log in or create a Jumia account."
             )}
->>>>>>> main
           </Typography>
         </div>
 
@@ -181,38 +157,17 @@ function Login_signup() {
             color="amber"
             fullWidth
           >
-<<<<<<< HEAD
-            Continue
-          </Button>
-          <Typography color="black" className="text-xs my-2 text-center">
-            By continuing you agree to Jumia&apos;s
-            <a href="#" className="block underline mt-1 text-orange-500">
-              Terms and Conditions
-=======
             {t("Continue")}
           </Button>
           <Typography color="black" className="text-xs my-2 text-center">
             {t("By continuing you agree to Jumia&apos;s")}
             <a href="#" className="block underline mt-1 text-orange-500">
               {t("Terms and Conditions")}
->>>>>>> main
             </a>
           </Typography>
         </form>
         <div className="w-[28rem]">
           <Button
-<<<<<<< HEAD
-            className="mt-5 mb-8 hover:bg-orange-800/5"
-            size="lg"
-            variant="outlined"
-            color="orange"
-            fullWidth
-          >
-            Login with Passkeys
-          </Button>
-          <Button
-=======
->>>>>>> main
             size="lg"
             variant="outlined"
             color="blue-gray"
@@ -220,14 +175,6 @@ function Login_signup() {
             onClick={() => loginWithGoogle()}
             fullWidth
           >
-<<<<<<< HEAD
-            <img src="https://docs.material-tailwind.com/icons/google.svg" alt="metamask" className="h-6 w-6" />
-            Continue with Google
-          </Button>
-          <Typography color="black" className="text-sm text-center">
-            For further support, you may visit the Help Center or contact our
-            customer service team.
-=======
             <img
               src="https://docs.material-tailwind.com/icons/google.svg"
               alt="metamask"
@@ -239,7 +186,6 @@ function Login_signup() {
             {t(
               "For further support, you may visit the Help Center or contact our customer service team."
             )}
->>>>>>> main
           </Typography>
           <div className="flex flex-col  items-center mt-5">
             <Image
@@ -261,13 +207,6 @@ export const loginSignup = (page) => page;
 Login_signup.getLayout = loginSignup;
 
 function Spinner() {
-<<<<<<< HEAD
-  return <div role="status" className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2">
-    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-orange-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>
-    <span className="sr-only">Loading...</span>
-  </div>
-}
-=======
   return (
     <div
       role="status"
@@ -301,4 +240,3 @@ export async function getStaticProps({ locale }) {
     },
   };
 }
->>>>>>> main
